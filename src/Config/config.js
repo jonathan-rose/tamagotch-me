ff8f2a5c14e06fd4a9c2785f08884fc9a15ca386
Jonathan Rose <jonathan_rose@outlook.com>
2023-07-08 16:19:55 +0100

    Start wobbleEgg function for random wobbling.

--- src/Config/config.js
+++ src/Config/config.js
@@ -7,7 +7,7 @@ export default {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            // gravity: { y: 300 },
             debug: false
         }
     }
