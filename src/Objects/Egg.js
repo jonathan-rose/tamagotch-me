0efaf83aa86d083097778a7c4cd11f48d69a9fda
Jonathan Rose <jonathan_rose@outlook.com>
2023-07-08 22:32:16 +0100

    Start lateral wobble that fits with the pixel art.

--- src/Objects/Egg.js
+++ src/Objects/Egg.js
@@ -1,5 +1,4 @@
 import 'phaser';
-import Pet from './Pet';
 
 export default class Egg extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
@@ -8,18 +7,31 @@ export default class Egg extends Phaser.GameObjects.Sprite {
         this.x = x;
         this.y = y + (this.height / 2);
 
-        this.wobbleINC = 0;
-        this.angleBase = (5);
-        this.angleMax = (10);
+        this.initX = x;
+
+        // this.wobbleINC = 0;
+        // this.angleBase = (5);
+        // this.angleMax = (10);
+
+        this.wobbleClickCount = 0;
 
         this.anims.create({
             key: 'crack',
             frames: this.anims.generateFrameNumbers('egg', { frames: [ 0, 1, 2 ] }),
             frameRate: 2,
-            repeat: 2,
+            repeat: 1,
             hideOnComplete: true,
         });
 
+        this.timer = this.scene.time.addEvent({
+            delay: 1000,
+            callback: this.wobbleEgg(),
+            callbackScope: this,
+            repeat: -1,
+            // loop: true
+            }
+        )
+S
         this.setDepth(0);
         this.setOrigin(0.5, 1);
 
@@ -30,36 +42,66 @@ export default class Egg extends Phaser.GameObjects.Sprite {
     }
 
     wobbleEgg () {
-        var wobbleAngle = Phaser.Math.Between(this.angleBase, this.angleMax);
+        const wobbleDuration = 250;
+        const wobbleMagnitude = 10;
 
         var wobbleDirection = Phaser.Math.RND.pick([-1, 1]);
 
         this.scene.tweens.add({
             targets: this,
-            angle: wobbleAngle * wobbleDirection,
-            duration: 100,
-            ease: 'Power0',
+            x: this.x - (wobbleMagnitude * wobbleDirection),
+            duration: wobbleDuration / 2,
+            // ease: 'Bounce.In',
             yoyo: true,
             repeat: 0,
+            onComplete: () => {
+                this.x = this.initX;
+            }
         });
 
-        this.wobbleINC += 5;
+        this.wobbleClickCount += 1;
 
-        console.log(this.angleBase, this.angleMax, this.wobbleINC);
-
-        if (this.angleMax < 25) {
-            this.angleBase += this.wobbleINC;
-            this.angleMax += this.wobbleINC;
-        } else {
-            this.angleMax = 30;
-        }
+        console.log(this.wobbleClickCount);
+        console.log(this.timer);
         
-
-        if (this.wobbleINC >= 70) {
+        if (this.wobbleClickCount >= 10) {
             this.scene.tweens.killTweensOf(this);
             this.play('crack').on('animationcomplete', this.onCrackComplete);
         }
+        
     }
+
+    // wobbleEgg () {
+    //     var wobbleAngle = Phaser.Math.Between(this.angleBase, this.angleMax);
+
+    //     var wobbleDirection = Phaser.Math.RND.pick([-1, 1]);
+
+    //     this.scene.tweens.add({
+    //         targets: this,
+    //         angle: wobbleAngle * wobbleDirection,
+    //         duration: 100,
+    //         ease: 'Power0',
+    //         yoyo: true,
+    //         repeat: 0,
+    //     });
+
+    //     this.wobbleINC += 5;
+
+    //     console.log(this.angleBase, this.angleMax, this.wobbleINC);
+
+    //     if (this.angleMax < 25) {
+    //         this.angleBase += this.wobbleINC;
+    //         this.angleMax += this.wobbleINC;
+    //     } else {
+    //         this.angleMax = 30;
+    //     }
+        
+
+    //     if (this.wobbleINC >= 70) {
+    //         this.scene.tweens.killTweensOf(this);
+    //         this.play('crack').on('animationcomplete', this.onCrackComplete);
+    //     }
+    // }
 
     onCrackComplete () {
         this.scene.setPetVisible();
