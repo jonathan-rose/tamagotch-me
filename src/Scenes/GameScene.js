0e03850f726d04f18e1e92538b43663ab6688451
Kimbsy <lordkimber@gmail.com>
2023-07-08 12:01:43 +0100

    Add pooping sounds

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import Util from '../Util';
 
 export default class GameScene extends Phaser.Scene {
     constructor () {
@@ -12,7 +13,7 @@ export default class GameScene extends Phaser.Scene {
 
         this.sys.game.globals.startTime = Date.now();
 
-        this.music = this.sound.add('music', { volume: 0.3, loop: true });
+        this.music = this.sound.add('music', { volume: 0.2, loop: true });
         this.sys.game.globals.musicTrack = this.music;
         if (this.model.musicOn === true && this.model.musicPlaying === false) {
             this.music.play();
@@ -23,6 +24,25 @@ export default class GameScene extends Phaser.Scene {
 
         this.add.image(400, 300, 'logo');
 
+    }
+
+    /**
+     * Play a random poo sound, 1% chance for super long poo
+     */
+    playPooSound() {
+        let options = [];
+        for (var i = 0; i < 33; i++) {
+            options.push('poo1');
+            options.push('poo2');
+            options.push('poo3');
+        }
+        options.push('poo-long');
+
+        if (this.model.soundOn === true)
+        {
+            let choice = Util.randNth(options);
+            this.sound.play(choice);
+        }
     }
 
     update ()
